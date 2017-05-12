a8af16d3a82d4271c213c344174a96dfcf109167
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2017-05-12 11:30:02 +0900

    zram: implement deduplication in zram
    This reverts commit 00fcab68ef36faecf1e3552708a0a4906f1b45be.

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -19,8 +19,10 @@
 #include <linux/zsmalloc.h>
 #include <linux/crypto.h>
 #include <linux/mm.h>
+#include <linux/spinlock.h>
 
 #include "zcomp.h"
+#include "zram_dedup.h"
 
 #define SECTORS_PER_PAGE_SHIFT	(PAGE_SHIFT - SECTOR_SHIFT)
 #define SECTORS_PER_PAGE	(1 << SECTORS_PER_PAGE_SHIFT)
@@ -63,6 +65,10 @@ enum zram_pageflags {
 /*-- Data structures */
 
 struct zram_entry {
+	struct rb_node rb_node;
+	u32 len;
+	u32 checksum;
+	unsigned long refcount;
 	unsigned long handle;
 };
 
@@ -117,6 +123,16 @@ struct zram_stats {
 	atomic64_t bd_objwrites;
 	atomic64_t lru_pages;
 #endif
+	atomic64_t dup_data_size;	/*
+					 * compressed size of pages
+					 * duplicated
+					 */
+	atomic64_t meta_data_size;	/* size of zram_entries */
+};
+
+struct zram_hash {
+	spinlock_t lock;
+	struct rb_root rb_root;
 };
 
 #ifdef CONFIG_ZRAM_LRU_WRITEBACK
@@ -180,6 +196,8 @@ struct zram {
 	struct zs_pool *mem_pool;
 	struct zcomp *comp;
 	struct gendisk *disk;
+	struct zram_hash *hash;
+	size_t hash_size;
 	/* Prevent concurrent execution of device init */
 	struct rw_semaphore init_lock;
 	/*
@@ -227,4 +245,6 @@ struct zram {
 	struct zram_writeback_buffer *buf;
 #endif
 };
+
+void zram_entry_free(struct zram *zram, struct zram_entry *entry);
 #endif
