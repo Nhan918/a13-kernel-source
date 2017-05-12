f0ab42600f70e63f4efd6a48b76e3aa64284f24a
Joonsoo Kim <iamjoonsoo.kim@lge.com>
2017-05-12 11:30:03 +0900

    zram: make deduplication feature optional
    This reverts commit 175ab469f004f57f359e5caec6850f60584eba0f.

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -216,6 +216,7 @@ struct zram {
 	 * zram is claimed so open request will be failed
 	 */
 	bool claim; /* Protected by bdev->bd_mutex */
+	bool use_dedup;
 #ifdef CONFIG_ZRAM_WRITEBACK
 	struct file *backing_dev;
 	spinlock_t wb_limit_lock;
@@ -245,6 +246,15 @@ struct zram {
 	struct zram_writeback_buffer *buf;
 #endif
 };
+
+static inline bool zram_dedup_enabled(struct zram *zram)
+{
+#ifdef CONFIG_ZRAM_DEDUP
+	return zram->use_dedup;
+#else
+	return false;
+#endif
+}
 
 void zram_entry_free(struct zram *zram, struct zram_entry *entry);
 #endif
