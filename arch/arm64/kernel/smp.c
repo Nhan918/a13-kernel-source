641692c1adecdf82b477197782c253bd6db3a1d5
Sultan Alsawaf <sultan@kerneltoast.com>
2020-11-24 20:04:58 -0800

    arch: lpm-levels: Remove idle prediction feature
    There is a lot of CPU time spent in hrtimer_try_to_cancel() when exiting
    idle according to perf. This is due to the idle prediction feature
    constantly arming and canceling a timer that it uses to track prediction
    accuracy. Idle prediction itself is used to try and select shallower
    idle states when an incoming wake-up is predicted in order to improve
    performance, but performance is actually better without idle prediction,
    presumably thanks to eliminating the hrtimer_try_to_cancel() overhead.
    
    The code for idle prediction is removed wholesale because the toggle
    doesn't cover everything, and it's easier to just nuke the whole feature
    than it is to try and cover everything with a toggle.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [ Tashar02: Forwardport to k4.19 ]
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    [ rsuntk: Only make it to arch ]

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -626,7 +626,6 @@ static void __init acpi_parse_and_init_cpus(void)
 DEFINE_PER_CPU(bool, pending_ipi);
 EXPORT_SYMBOL_GPL(pending_ipi);
 
-static void (*__smp_update_ipi_history_cb)(int cpu);
 /*
  * Enumerate the possible CPU set from the device tree and build the
  * cpu logical map array containing MPIDR values related to logical
@@ -784,12 +783,6 @@ void __init set_smp_cross_call(void (*fn)(const struct cpumask *, unsigned int))
 {
 	__smp_cross_call = fn;
 }
-
-void set_update_ipi_history_callback(void (*fn)(int))
-{
-	__smp_update_ipi_history_cb = fn;
-}
-EXPORT_SYMBOL_GPL(set_update_ipi_history_callback);
 
 static const char *ipi_types[NR_IPI] __tracepoint_string = {
 #define S(x,s)	[x] = s
@@ -1012,8 +1005,6 @@ void handle_IPI(int ipinr, struct pt_regs *regs)
 
 void smp_send_reschedule(int cpu)
 {
-	if (__smp_update_ipi_history_cb)
-		__smp_update_ipi_history_cb(cpu);
 	smp_cross_call_common(cpumask_of(cpu), IPI_RESCHEDULE);
 }
 
