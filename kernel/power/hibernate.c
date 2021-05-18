bae03525b6aaf19b29b4bb5ecc1f1947354d8ed1
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-19 02:32:09 +0530

    kernel: power: omit trace markers and logspam
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -714,7 +714,6 @@ int hibernate(void)
 		goto Unlock;
 	}
 
-	pr_info("hibernation entry\n");
 	pm_prepare_console();
 	error = __pm_notifier_call_chain(PM_HIBERNATION_PREPARE, -1, &nr_calls);
 	if (error) {
@@ -722,9 +721,7 @@ int hibernate(void)
 		goto Exit;
 	}
 
-	pr_info("Syncing filesystems ... \n");
 	ksys_sync();
-	pr_info("done.\n");
 
 	error = freeze_processes();
 	if (error)
@@ -785,7 +782,6 @@ int hibernate(void)
 	atomic_inc(&snapshot_device_available);
  Unlock:
 	unlock_system_sleep();
-	pr_info("hibernation exit\n");
 
 	return error;
 }
