199f9fb8128116253e8b29199fd1ccc1e8c9b21e
Faris <90097027+rsuntk@users.noreply.github.com>
2025-10-25 03:02:56 +0000

    cpufreq: stats: Align with https://github.com/user-why-red/android_kernel_xiaomi_sdm660_419
    Signed-off-by: Faris <90097027+rsuntk@users.noreply.github.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -281,7 +281,7 @@ void cpufreq_stats_record_transition(struct cpufreq_policy *policy,
 	new_index = freq_table_get_index(stats, new_freq);
 
 	/* We can't do stats->time_in_state[-1]= .. */
-	if (old_index == -1 || new_index == -1 || old_index == new_index)
+	if (unlikely(old_index == -1 || new_index == -1 || old_index == new_index))
 		return;
 
 	cpufreq_stats_update(stats, stats->last_time);
