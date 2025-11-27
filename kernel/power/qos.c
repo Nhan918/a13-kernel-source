cca42d8d5426407bbec69b487f28946e1635bd16
Nhan918 <truyennhan2910@gmail.com>
2025-11-27 19:29:47 +0700

    fix error

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -1229,17 +1229,6 @@ void pm_qos_remove_request(struct pm_qos_request *req)
 		return;
 	}
 
-#ifdef CONFIG_SMP
-	if (req->type == PM_QOS_REQ_AFFINE_IRQ) {
-		int ret = 0;
-		/* Get the current affinity */
-		ret = irq_set_affinity_notifier(req->irq, NULL);
-		if (ret)
-			WARN(1, "IRQ affinity notify set failed\n");
-	}
-#endif
->>>>>>> 6f23950978f5 (qos: Remove pm_qos_update_request_timeout() API)
-
 	trace_pm_qos_remove_request(req->pm_qos_class, PM_QOS_DEFAULT_VALUE);
 	pm_qos_update_target(pm_qos_array[req->pm_qos_class]->constraints,
 			     &req->node, PM_QOS_REMOVE_REQ,
